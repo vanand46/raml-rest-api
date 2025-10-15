cc03c5a002a822867f8352da9ada37cbe66f29a6
Anand V <42205127+vanand46@users.noreply.github.com>
2025-10-15 15:06:56 +0530

    Added/updated end point for post, put, delete, getCustomerbyid

--- src/routes/customer.routes.ts
+++ src/routes/customer.routes.ts
@@ -1,17 +1,17 @@
-import { Router } from 'express';
+import { Router, RequestHandler } from 'express';
 import * as customerController from '../controllers/customer.controller';
 import { createCustomerDto } from '../dto/create-customer.dto';
 import { handleValidation } from '../middlewares/validation.middleware';
 
 const router = Router();
 
-router.post(
-    '/',
-    createCustomerDto,
-    handleValidation,
-    customerController.createCustomer
-);
+const validateCustomer: RequestHandler[] = [...createCustomerDto, handleValidation];
 
+// Routes
+router.post('/', validateCustomer, customerController.createCustomer);
+router.put('/:id', validateCustomer, customerController.updateCustomer);
 router.get('/', customerController.getCustomers);
+router.get('/:id', customerController.getCustomer);
+router.delete('/:id', customerController.deleteCustomer);
 
 export default router;
